7974c3a988ffb793143e5bbaecf98f7253249486
Silas Reinagel <silas.reinagel@gmail.com>
2023-12-01 12:24:31 -0600

    Remove Wishlist Item

---
+++ src/src/components/ConfirmModal.svelte
@@ -0,0 +1,92 @@
+<script>
+  export let showModal = false;
+  export let title = "Confirm Action";
+  export let description = "Are you sure you want to perform this action?";
+  export let onConfirm = () => {};
+
+  const confirmAction = () => {
+    showModal = false;
+    onConfirm();
+  };
+
+  const cancelAction = () => {
+    showModal = false;
+  };
+</script>
+
+<style>
+.modal {
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.7);
+  opacity: 0;
+  animation: fadeIn 0.3s forwards;
+  z-index: 10;
+}
+
+.modal-content {
+  background: #2c3e50;
+  color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.5);
+  transform: translateY(-20%);
+  animation: slideIn 0.3s forwards;
+}
+
+@keyframes fadeIn {
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes slideIn {
+  to {
+    transform: translateY(0);
+  }
+}
+
+.modal-buttons {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20px;
+}
+
+button {
+  background-color: #3498db;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  margin: 0 5px;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+}
+
+button:hover {
+  background-color: #2980b9;
+}
+
+button:active {
+  transform: translateY(1px);
+}
+</style>
+
+{#if showModal}
+  <div class="modal">
+    <div class="modal-content">
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <div class="modal-buttons">
+        <button on:click={confirmAction}>Confirm</button>
+        <button on:click={cancelAction}>Cancel</button>
+      </div>
+    </div>
+  </div>
+{/if}
--- src/src/pages/MyWishlist.svelte
+++ src/src/pages/MyWishlist.svelte
@@ -2,14 +2,31 @@
   import { onMount } from "svelte";
   import { wishlistItems } from "../appState";
   import { currentUser } from "../appState";
-  import { initWishlistItemsDatabase } from "../datasource";
+  import { deleteWishlistItem, initWishlistItemsDatabase } from "../datasource";
   import ChristmasButton from "../components/ChristmasButton.svelte";
   import AddWishlistItemForm from "../components/WishlistItemForm.svelte";
   import BackButton from "../components/BackButton.svelte";
+    import ConfirmModal from "../components/ConfirmModal.svelte";
 
   let currentUserId = $currentUser.itemId;
   let wishlist = $wishlistItems.filter(i => i.item.userId === currentUserId)
+  let confirmModalAction = undefined;
+  let selectedItem = undefined;
   console.log('Wishlist', { currentUser: $currentUser.item.displayName, currentUserId, wishlist });
+
+  const onDeleteClick = (item) => {
+    selectedItem = item;
+    confirmModalAction = () => deleteWishlistItem(item.itemId)
+      .then(() => {
+        confirmModalAction = undefined
+        selectedItem = undefined;
+      })
+      .catch(e => {
+        confirmModalAction = undefined
+        selectedItem = undefined;
+        console.log("Error deleting wishlist item", e);
+      });
+  }
 
   onMount(async () => {
     await initWishlistItemsDatabase();
@@ -21,6 +38,12 @@
 </script>
 
 <div class="full-width col center">
+  <ConfirmModal 
+    onConfirm={confirmModalAction} 
+    showModal={!!confirmModalAction}
+    title="Remove Wishlist Item"
+    description={`Are you sure you want to remove ${selectedItem?.item?.displayName ?? ""} from your wishlist?`}
+  />
   <div class="full-width">
     <BackButton />
   </div>
@@ -30,11 +53,16 @@
       <h3>You have no items on your wishlist</h3>
     {/if}
     <div class="flex-rows">
-      {#each wishlist as item}
+      {#each wishlist as row}
         <ChristmasButton 
-          text={item.item.displayName}
-          number={item.item.maxGivers}
-          url={item.item.url} />
+          text={row.item.displayName}
+          number={row.item.maxGivers}
+          url={row.item.url}
+        >
+          <button slot="action" class="red-sphere-button center pointer" on:click={() => onDeleteClick(row)}>
+            <div>X</div>
+          </button>
+        </ChristmasButton>
       {/each}
     </div>
   </div>
